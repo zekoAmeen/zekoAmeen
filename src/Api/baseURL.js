12f0b1e4672e98901ffd5c4c18c6aab2769b98c8
zekoAmeen <157914355+zekoAmeen@users.noreply.github.com>
2024-01-28 22:38:59 +0300

    Update baseURL.js

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 
-const baseUrl = axios.create({ baseURL: "https://zeko2024.onrender.com" })
+const baseUrl = axios.create({ baseURL: "https://zeko022.onrender.com" })
 
 export default baseUrl
